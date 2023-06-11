cce8b9053830374a5446c8c5c40f73fb2143908d
luhaifeng666 <youzui@hotmail.com>
2023-06-11 21:49:45 +0800

    build: vitepress项目初始化

---
+++ .vitepress/config.ts
@@ -0,0 +1,32 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "Bunpou",
+  description: "用于查阅日语语法",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Examples', link: '/markdown-examples' }
+    ],
+
+    search: {
+      provider: 'local'
+    },
+
+    sidebar: [
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ]
+  }
+})
