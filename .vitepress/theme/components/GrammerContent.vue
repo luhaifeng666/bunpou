9e0e1033559cfe63059a495bebc22b644962786e
luhaifeng666 <youzui@hotmail.com>
2023-06-29 00:25:01 +0800

    feat: 文件名统一&修改语法组件转换逻辑&添加疑问词术语文档

--- .vitepress/theme/components/GrammerContent.vue
+++ .vitepress/theme/components/GrammerContent.vue
@@ -2,7 +2,7 @@
  * @Author: luhaifeng666 youzui@hotmail.com
  * @Date: 2023-06-14 08:39:33
  * @LastEditors: luhaifeng666
- * @LastEditTime: 2023-06-27 00:18:24
+ * @LastEditTime: 2023-06-29 00:15:21
  * @Description: 
 -->
 <template>
@@ -18,12 +18,11 @@ const props = defineProps({
 })
 
 const sentenceElement = computed(() => {
+  const { sentence, trans } = props
   // 转换 ruby & strong 标签
-  const _sentence = props.sentence.replace(/\[([^\[]*)\/([\u3040-\u309F\u30A0-\u30FF\u31F0-\u31FF]*)\]/g, word => {
+  return `${sentence} ${trans}`.replace(/\[([^\[]*)\/([\u3040-\u309F\u30A0-\u30FF\u31F0-\u31FF]*)\]/g, word => {
     const [rb, rt] = word.replace(/\[|\]/g, '').split('/')
     return `<ruby><rb>${rb}</rb><rt>${rt}</rt></ruby>`
-  }).replace(/(\*\*)([^*]*)(\*\*)/g, '<strong style="color: #fb923c">$2</strong>')
-
-  return _sentence + props.trans
+  }).replace(/\*\*(.*?)\*\*/g, '<strong style="color: #fb923c">$1</strong>')
 })
 </script>
