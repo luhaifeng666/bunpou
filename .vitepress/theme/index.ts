9e07fcdd10aa60fde8a633ba2cea2831f619f114
luhaifeng666 <youzui@hotmail.com>
2023-08-10 11:04:56 +0800

    style: 修改下划线样式

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,16 +1,17 @@
 /*
  * @Author: luhaifeng666 youzui@hotmail.com
  * @Date: 2023-06-14 08:38:26
- * @LastEditors: luhaifeng666
- * @LastEditTime: 2023-06-14 22:23:38
- * @Description: 
+ * @LastEditors: luhaifeng666 youzui@hotmail.com
+ * @LastEditTime: 2023-08-10 11:02:48
+ * @Description:
  */
-import DefaultTheme from 'vitepress/theme'
-import GrammerContent from './components/GrammerContent.vue'
+import DefaultTheme from "vitepress/theme";
+import GrammerContent from "./components/GrammerContent.vue";
+import "./custom.css";
 
 export default {
-  extends: DefaultTheme,
-  enhanceApp(ctx) {
-    ctx.app.component('GrammerContent', GrammerContent)
-  }
-}
+	extends: DefaultTheme,
+	enhanceApp(ctx) {
+		ctx.app.component("GrammerContent", GrammerContent);
+	},
+};
