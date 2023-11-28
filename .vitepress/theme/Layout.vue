b641e6a6f5eadb560643deaf9889895acda0725b
luhaifeng666 <youzui@hotmail.com>
2023-11-28 14:59:57 +0800

    build: 添加评论功能

---
+++ .vitepress/theme/Layout.vue
@@ -0,0 +1,23 @@
+<!--
+ * @Author: luhaifeng666 youzui@hotmail.com
+ * @Date: 2023-11-28 14:44:29
+ * @LastEditors: luhaifeng666 youzui@hotmail.com
+ * @LastEditTime: 2023-11-28 14:44:56
+ * @FilePath: /bunpou/.vitepress/theme/Layout.vue
+ * @Description: 
+ * 
+-->
+<template>
+	<Layout>
+		<template #doc-after>
+			<Comment style="margin-top: 24px" />
+		</template>
+	</Layout>
+</template>
+
+<script lang="ts" setup>
+import DefaultTheme from "vitepress/theme";
+import Comment from "./components/Comment.vue";
+
+const { Layout } = DefaultTheme;
+</script>
