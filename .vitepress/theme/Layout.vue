6bc1f497ce6e6c70d7994bdfa14f50eecb6c480f
luhaifeng666 <youzui@hotmail.com>
2023-11-29 12:19:32 +0800

    feat: 修改评论插件为GISCUS

--- .vitepress/theme/Layout.vue
+++ .vitepress/theme/Layout.vue
@@ -2,7 +2,7 @@
  * @Author: luhaifeng666 youzui@hotmail.com
  * @Date: 2023-11-28 14:44:29
  * @LastEditors: luhaifeng666 youzui@hotmail.com
- * @LastEditTime: 2023-11-28 14:44:56
+ * @LastEditTime: 2023-11-29 11:18:28
  * @FilePath: /bunpou/.vitepress/theme/Layout.vue
  * @Description: 
  * 
@@ -10,14 +10,16 @@
 <template>
 	<Layout>
 		<template #doc-after>
-			<Comment style="margin-top: 24px" />
+			<!-- <Comment style="margin-top: 24px" /> -->
+			<GiscusComment />
 		</template>
 	</Layout>
 </template>
 
 <script lang="ts" setup>
 import DefaultTheme from "vitepress/theme";
-import Comment from "./components/Comment.vue";
+// import Comment from "./components/Comment.vue";
+import GiscusComment from "./components/GiscusComment.vue";
 
 const { Layout } = DefaultTheme;
 </script>
---
+++ .vitepress/theme/components/GiscusComment.vue
@@ -0,0 +1,86 @@
+<!--
+ * @Author: luhaifeng666 youzui@hotmail.com
+ * @Date: 2023-11-29 11:07:39
+ * @LastEditors: luhaifeng666 youzui@hotmail.com
+ * @LastEditTime: 2023-11-29 12:18:36
+ * @FilePath: /bunpou/.vitepress/theme/components/GiscusComment.vue
+ * @Description: 
+ * 
+-->
+
+<template>
+	<section ref="comments" style="margin-top: 40px"></section>
+</template>
+
+<script setup>
+import { onMounted, onUnmounted, ref, toRef, watch } from "vue";
+import { useData } from "vitepress";
+
+const isDark = toRef(useData(), "isDark");
+const page = toRef(useData(), "page");
+const comments = ref();
+
+const setComments = () => {
+	const script = document.createElement("script");
+	const configs = {
+		"data-repo": "luhaifeng666/bunpou",
+		"data-repo-id": "R_kgDOJszpjg",
+		"data-category": "Q&A",
+		"data-category-id": "DIC_kwDOJszpjs4CaJ8g",
+		"data-mapping": "pathname",
+		"data-strict": "1",
+		"data-reactions-enabled": "1",
+		"data-emit-metadata": "0",
+		"data-input-position": "bottom",
+		"data-lang": "zh-CN",
+		"data-theme": "preferred_color_scheme",
+		crossorigin: "anonymous",
+		async: true,
+	};
+	script.src = "https://giscus.app/client.js";
+	Object.entries(configs).forEach(([key, value]) => {
+		script.setAttribute(key, value);
+	});
+
+	comments.value && (comments.value.innerHTML = "");
+	comments.value.appendChild(script);
+};
+
+const setTheme = () => {
+	const theme = isDark.value ? "preferred_color_scheme" : "light";
+	const iframe = document.querySelector("iframe.giscus-frame");
+	if (iframe) {
+		iframe.contentWindow?.postMessage(
+			{
+				giscus: {
+					setConfig: {
+						theme: `https://giscus.app/themes/${theme}.css`,
+					},
+				},
+			},
+			"https://giscus.app"
+		);
+	}
+};
+
+const watchMsg = (msg) => {
+	if (msg.origin === "https://beaudar.lipk.org") {
+		setTheme();
+	}
+};
+
+watch(page, (val) => {
+	val.relativePath && val.relativePath !== "index.md" && setComments();
+});
+
+watch(isDark, setTheme);
+
+onMounted(() => {
+	setComments();
+	window.addEventListener("message", watchMsg);
+});
+
+onUnmounted(() => {
+	window.removeEventListener("message", watchMsg);
+});
+</script>
