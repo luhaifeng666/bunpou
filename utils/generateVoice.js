8b2ad3e1846001d8410e79981bad13d1927f96dd
luhaifeng666 <youzui@hotmail.com>
2024-04-10 19:34:50 +0800

    feat: 添加mdast解析

--- utils/generateVoice.js
+++ utils/generateVoice.js
@@ -1,7 +1,13 @@
 import { generateVoice } from "./speech.js";
+import fs from 'node:fs/promises'
+import path from "node:path";
+import { fromMarkdown } from 'mdast-util-from-markdown'
+
+const doc = await fs.readFile(path.resolve('docs/bunpou/course1/1-2-1.md'))
+const tree = fromMarkdown(doc)
+
+console.log(JSON.stringify(tree))
 
 // 1. 获取所有语法目录
 // 2. 获取当前已经存在的语音文件
 // 3. 去重 & 生成新的语音文件
-
-console.log(!23123)
