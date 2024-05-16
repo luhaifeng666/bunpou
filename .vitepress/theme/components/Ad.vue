d6717a2fc95572444b14ed52ad877def0a09f45c
haifeng.lu <haifeng.lu@ly.com>
2024-05-16 15:49:59 +0800

    chore: 添加国内站点无法访问的站点提示

--- .vitepress/theme/components/Ad.vue
+++ .vitepress/theme/components/Ad.vue
@@ -1,3 +1,44 @@
 <template>
-    <div>广告位招租</div>
+	<div class="bunpou-ad">
+		<img src="../../../public/imgs/BP.svg" alt="" />
+		<p class="bunpou-tip">
+			因 Gitee Pages
+			服务维护调整，导致国内站点无法部署，因此短期内暂时不支持访问国内站点~
+		</p>
+		<p class="bunpou-bb">
+			トラフィックはほとんどありませんが、私の広告スペースのレンタルに影響を与えることはできません...
+		</p>
+	</div>
 </template>
+
+<style>
+.bunpou-ad {
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	padding: 24px;
+	border-radius: 12px;
+	min-height: 256px;
+	text-align: center;
+	line-height: 18px;
+	font-size: 12px;
+	font-weight: 500;
+	background-color: var(--vp-carbon-ads-bg-color);
+}
+.bunpou-ad > img {
+	width: 40%;
+	display: block;
+	margin-bottom: 10px;
+}
+.bunpou-tip {
+	text-align: justify;
+	font-weight: bold;
+	color: var(--vp-code-color);
+}
+.bunpou-bb {
+	font-size: 10px;
+	margin-top: 10px;
+	color: var(--vp-c-text-3);
+}
+</style>
