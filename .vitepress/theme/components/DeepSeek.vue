1e966885b1d8e5dd14de31b3639585efa3399009
haifeng.lu <haifeng.lu@ly.com>
2025-02-24 22:27:04 +0800

    feat: 新增DeepSeek组件，但是DS目前关闭充值了，用不了，等回头能用的时候再说吧

---
+++ .vitepress/theme/components/DeepSeek.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="bunpou-ds"></div>
+</template>
+
+<script setup>
+import OpenAI from "openai";
+import { onMounted } from 'vue'
+
+onMounted(() => {
+    main()
+})
+
+const openai = new OpenAI({
+        dangerouslyAllowBrowser: true,
+        baseURL: 'https://api.deepseek.com',
+        apiKey: '' // TODO 设置APIKEY
+});
+
+async function main() {
+  const completion = await openai.chat.completions.create({
+    messages: [{ role: "system", content: "You are a helpful assistant." }],
+    model: "deepseek-chat",
+  });
+
+  console.log(completion);
+}
+
+main();
+</script>
